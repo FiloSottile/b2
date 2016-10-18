c32038dd691290351948e8fbe956b0ade65e70ea
Filippo Valsorda <hi@filippo.io>
2016-10-18 11:32:26 +0200

    Add tests for non-Seek Upload types

--- upload_test.go
+++ upload_test.go
@@ -3,6 +3,7 @@ package b2_test
 import (
 	"bytes"
 	"crypto/rand"
+	"io"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -55,5 +56,50 @@ func TestUploadFile(t *testing.T) {
 	defer c.DeleteFile(fi.ID, fi.Name)
 	if fi.ContentLength != 123456 {
 		t.Error("mismatched fi.ContentLength", fi.ContentLength)
+	}
+	if n, err := io.Copy(ioutil.Discard, f); err != nil || n != 0 {
+		t.Error("should have read 0 bytes:", n, err)
+	}
+}
+
+func TestUploadBuffer(t *testing.T) {
+	c := getClient(t)
+	b := getBucket(t, c)
+	defer b.Delete()
+
+	content := make([]byte, 123456)
+	rand.Read(content)
+	buf := bytes.NewBuffer(content)
+	fi, err := b.Upload(buf, "foo-file", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.DeleteFile(fi.ID, fi.Name)
+	if fi.ContentLength != 123456 {
+		t.Error("mismatched fi.ContentLength", fi.ContentLength)
+	}
+	if buf.Len() != 0 {
+		t.Error("Buffer is not empty")
+	}
+}
+
+func TestUploadReader(t *testing.T) {
+	c := getClient(t)
+	b := getBucket(t, c)
+	defer b.Delete()
+
+	content := make([]byte, 123456)
+	rand.Read(content)
+	r := bytes.NewReader(content)
+	fi, err := b.Upload(ioutil.NopCloser(r), "foo-file", "") // shadow Seek method
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.DeleteFile(fi.ID, fi.Name)
+	if fi.ContentLength != 123456 {
+		t.Error("mismatched fi.ContentLength", fi.ContentLength)
+	}
+	if r.Len() != 0 {
+		t.Error("Reader is not empty")
 	}
 }
